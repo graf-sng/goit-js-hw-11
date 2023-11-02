98fc0c618af2f9e4279c5a8df46d3d566409d5d5
Voinash Denys <desdenisandqwerty@gmail.com>
2023-11-02 11:27:23 +0200

    add parcel

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import { Notify } from "notiflix/build/notiflix-notify-aio";
 const elements = {
   form: document.querySelector(".search-form"),
 };
+
 elements.form.addEventListener("submit", handlerSubmit);
+
 function handlerSubmit(evt) {
   evt.preventDefault();
   const search = evt.currentTarget.elements.searchQuery.value;
