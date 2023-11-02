06e3125d3e17066b92550c8cb713392178f0a64a
Voinash Denys <desdenisandqwerty@gmail.com>
2023-11-02 18:51:44 +0200

    test simple

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 import { Notify } from "notiflix/build/notiflix-notify-aio";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 const elements = {
   form: document.querySelector(".search-form"),
